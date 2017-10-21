ff11ae6cf5f28d4d558d445ccfdb87094f1c75bf
Sam <sam@s-blu.de>
2017-10-21 19:58:22 +0200

    feat(*): make material usable, fix npm unmet peer deps

--- src/app/alrtw-material/alrtw-material.module.ts
+++ src/app/alrtw-material/alrtw-material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatButtonModule, MatCheckboxModule, MatInputModule} from '@angular/material';
-import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-const materialModules = [MatButtonModule, MatCheckboxModule, MatInputModule, NoopAnimationsModule];
+const materialModules = [CommonModule, BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatInputModule,
+  MatFormFieldModule];
 
 @NgModule({
   imports: materialModules,
